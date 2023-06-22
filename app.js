ca8d62ef61e876cf426103c8515241069ac486b4
i-lili <113575743+i-lili@users.noreply.github.com>
2023-06-22 09:00:00 +0200

    Logging Requests and Errors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const auth = require('./middlewares/auth');
 const { validateUser, validateLogin } = require('./middlewares/validation');
 const { NotFoundError } = require('./errors/Errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const port = 3000;
@@ -17,6 +18,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+app.use(requestLogger); // подключаем логгер запросов
 
 app.use(express.json());
 app.use(cookieParser());
@@ -33,9 +36,11 @@ app.use('*', (req, res, next) => {
   next(new NotFoundError('Запрашиваемый ресурс не найден'));
 });
 
-app.use(errors());
+app.use(errors()); // обработчик ошибок celebrate
 
-// Обработка ошибок
+app.use(errorLogger); // подключаем логгер ошибок
+
+// централизованный обработчик ошибок
 app.use(errorHandler);
 
 app.listen(port);
